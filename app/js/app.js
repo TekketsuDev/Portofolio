414fb36871cfd3526cb3db3315ee65f222c2c094
TekketsuHen <vjamet15@gmail.com>
2022-12-25 17:46:55 +0100

    update v0

---
+++ app/js/app.js
@@ -0,0 +1,4 @@
+var bar1 = new ldBar("#mibarra");
+/* ldBar stored in the element */
+var bar2 = document.getElementById('mibarra').ldBar;
+bar1.set(50);
---
+++ bash/newPartial.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+    #Define variables
+    currentDir=$(pwd) 
+    echo "$currentDir"
+
+    read newPartial
+    
+
+    searchDir(){
+        for existingDir in "${allPartials[@]}"; do
+            echo "$existingDir"
+        done
+    }
+
+    #find allPartials
+
+    findPartials(){
+        
+            if [ -e $newPartial ]
+            then
+                echo "element $newPartial"
+            else
+                echo "file is not a directory"
+
+            fi
+        
+    }
+    allPartials="$(findPartials)"
+
+
+    #create partial folder if the dir dont exist
+        echo "this is a new line" >> "$existingDir"/"$newPartial".scss
