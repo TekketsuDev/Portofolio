0e14a8af574270137e08f0db9a631249d001ebbd
TekketsuHen <vjamet15@gmail.com>
2023-01-19 12:48:11 +0100

    update 19/01

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,3 @@
-var bar1 = new ldBar("#mibarra");
-/* ldBar stored in the element */
-var bar2 = document.getElementById('mibarra').ldBar;
-bar1.set(50);
+function toggleMobileMenu(menu) {
+    menu.classList.toggle('open');
+}
