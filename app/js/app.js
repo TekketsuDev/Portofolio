62d97182c7017de339f76dddddf841f3b8966ec0
TekketsuHen <vjamet15@gmail.com>
2023-01-31 18:39:54 +0100

    update 31/01

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,15 @@
 function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
 }
+
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("site-header").style.top = "0";
+  } else {
+    document.getElementById("site-header").style.top = "-103px";
+  }
+  prevScrollpos = currentScrollPos;
+}
